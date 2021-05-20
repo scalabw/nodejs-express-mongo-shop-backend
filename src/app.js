b854dffa0a4c9c1d4b983494db4f42a6c4addfde
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-20 10:54:53 +0200

    adding several elements to the middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log('Request received')
+  next()
+})
+
+app.use((req, res, next) => {
+  res.status(201)
+  next()
+})
+
+app.use((req, res, next) => {
   res.json({ message: 'Request received !' })
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('Response sent')
+  next()
 })
 
 module.exports = app
