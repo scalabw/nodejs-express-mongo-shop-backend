e0b52c9305868b158d355511d65f4a3ae5c01e2a
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-20 10:48:03 +0200

    correcting wording

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use((req, res) => {
-  res.json({ message: 'Votre requête a bien été reçue !' })
+  res.json({ message: 'Request received !' })
 })
 
 module.exports = app
