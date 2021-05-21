82f9370fb7fa42ee483a892f3f65dedac008a28c
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-21 19:26:02 +0200

    method Update + Delete implemented with mongoDB

--- src/app.js
+++ src/app.js
@@ -29,16 +29,16 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }))
-})
-
 app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then((thing) => res.status(200).json(thing))
     .catch((error) => res.status(404).json({ error }))
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet deleted !' }))
+    .catch((error) => res.status(400).json({ error }))
 })
 
 app.post('/api/stuff', (req, res, next) => {
@@ -51,4 +51,17 @@ app.post('/api/stuff', (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Objet created !' }))
     .catch((error) => res.status(400).json({ error }))
 })
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modified !' }))
+    .catch((error) => res.status(400).json({ error }))
+})
+
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }))
+})
+
 module.exports = app
