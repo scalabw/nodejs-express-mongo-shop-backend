25e863cb5b68434b7ee27c9d29b2b42b28e35946
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-22 23:12:47 +0200

    code improvment: creation of routes + controllers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const Thing = require('./models/thing')
+const stuffRoutes = require('./routes/stuff')
 
 require('dotenv').config()
 mongoose
@@ -17,8 +17,6 @@ mongoose
 
 const app = express()
 
-app.use(express.json())
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
@@ -29,38 +27,8 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }))
-})
+app.use(express.json())
 
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet deleted !' }))
-    .catch((error) => res.status(400).json({ error }))
-})
+app.use('/api/stuff', stuffRoutes)
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id
-  const thing = new Thing({
-    ...req.body,
-  })
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: 'Objet created !' }))
-    .catch((error) => res.status(400).json({ error }))
-})
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modified !' }))
-    .catch((error) => res.status(400).json({ error }))
-})
-
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }))
-})
 module.exports = app
---
+++ src/controllers/stuff.js
@@ -0,0 +1,36 @@
+const Thing = require('../models/thing')
+
+exports.getAllStuff = (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.createThing = (req, res, next) => {
+  delete req.body._id
+  const thing = new Thing({
+    ...req.body,
+  })
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: 'Objet created !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.getOneThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }))
+}
+
+exports.modifyThing = (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modified !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet deleted !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
