6ed47848831f3d8f1c50b65cd84995234b2d83e3
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-23 23:50:41 +0200

    implemetation of route register + login done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
 
 require('dotenv').config()
 mongoose
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 app.use(express.json())
 
 app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
