17c08c5fb4894b85df61fcc96f6bce5670f3ff46
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-24 23:15:39 +0200

    handling reception of images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const stuffRoutes = require('./routes/stuff')
 const userRoutes = require('./routes/user')
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes)
 
