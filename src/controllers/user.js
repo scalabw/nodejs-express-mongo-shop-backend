28b60229509d52ac141adceb6852f40d6eef165c
William Scalabre <wscalabr@bouyguestelecom.fr>
2021-05-24 21:40:33 +0200

    implemetation of real webtoken on login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,19 +1,20 @@
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
 exports.signup = (req, res, next) => {
-  exports.signup = (req, res, next) => {
-    bcrypt
-      .hash(req.body.password, 10)
-      .then((hash) => {
-        const user = new User({
-          email: req.body.email,
-          password: hash,
-        })
-        user
-          .save()
-          .then(() => res.status(201).json({ message: 'User created with success !' }))
-          .catch((error) => res.status(400).json({ error }))
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
       })
-      .catch((error) => res.status(500).json({ error }))
-  }
+      user
+        .save()
+        .then(() => res.status(201).json({ message: 'User created with success !' }))
+        .catch((error) => res.status(400).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }))
 }
 
 exports.login = (req, res, next) => {
@@ -30,7 +31,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: 'TOKEN',
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }),
           })
         })
         .catch((error) => res.status(500).json({ error }))
